168a6b724f7beca4047e7102484cb9daa0c6cf5b
Mikhail Zhyhariev <zhygarevmikhail@gmail.com>
2018-04-15 22:48:19 +0300

    Some refactor exp filter

--- filters/exponential/exponential_filter.c
+++ filters/exponential/exponential_filter.c
@@ -12,7 +12,7 @@
  * @param  previous_data - previous data value
  * @return               filtered data
  */
-float getExpFilteredAngle(float data, float previous_data) {
+float EXP_getExpFilteredAngle(float data, float previous_data) {
     return LAMBDA * data + (1 - LAMBDA) * previous_data;
 }
 
@@ -23,10 +23,10 @@ float getExpFilteredAngle(float data, float previous_data) {
  * @param  len           - an array length
  * @return               an array of filtered data
  */
-float* getExpFilteredAngles(float* data, float* previous_data, unsigned char len) {
+float* EXP_getExpFilteredAngles(float* data, float* previous_data, unsigned char len) {
     float* ret = (float *)malloc(sizeof(float) * len);
     for (unsigned char i = 0; i < len; i++) {
-        ret[i] = getExpFilteredAngle(data[i], previous_data[i]);
+        ret[i] = EXP_getExpFilteredAngle(data[i], previous_data[i]);
     }
     return ret;
 }
